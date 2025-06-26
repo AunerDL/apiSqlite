c46e04bf1653b080b9e8e1304da4acba88130823
AunerDL <gabodeadlivef2@gmail.com>
2025-06-25 18:07:49 -0600

    se modifica lo de la base de datos

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./datos.db"
+import os
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////tmp/datos.db")
+
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
